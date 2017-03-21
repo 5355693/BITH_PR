f899b9e819db326018b34de11948c30976b1ab3f
John Lloyd <5355693@gmail.com>
2017-03-21 17:16:45 -0400

    Analysis script update

--- Analysis_script.R
+++ Analysis_script.R
@@ -1,5 +1,8 @@
 library(ggplot2)
 cells <- read.csv(file = "Data/Cells_surveyed.csv", header = T)
 ggplot(data = cells, aes(x = mean, fill = factor(BITH))) + geom_dotplot(method="histodot",binwidth = 0.085,stackgroups = TRUE)
-ggplot(data = cells, aes(x = mean, fill = BITH_cat)) + geom_histogram() + xlab("Predicted probability of Bicknell's\nThrush presence") + 
-  ylab("Number of areas surveyed") + guides(fill=guide_legend(title="Bicknell's Thrush\ndetected?"))
+png(file = "/Users/johnlloyd/Dropbox/VCE/BITH Puerto Rico/Figure3.png")
+ggplot(data = cells, aes(x = mean, fill = BITH_cat)) + geom_histogram() + xlab("Predicted probability of Bicknell's\nThrush presence in survey block") + 
+  ylab("Number of blocks") + guides(fill=guide_legend(title="Bicknell's Thrush\ndetected?"))
+dev.off()
+summary(cells)
